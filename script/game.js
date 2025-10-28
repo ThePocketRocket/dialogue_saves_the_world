0917fb220783d882a89a5f6812f43d54b26b9c0d
Daniel Ferreira <pocketrocket@outlook.com.br>
2025-10-28 02:19:55 -0300

    Add: Arquivos iniciais

---
+++ script/game.js
@@ -0,0 +1,50 @@
+const words = {
+  1: ["CASA", "BOLA", "GATO", "PATO", "FOGO", "SOL", "FLOR", "MÃO", "PÉ", "NUVEM", "LUA", "RATO", "MESA", "LEÃO", "UVA", "VACA", "TREM"],
+  2: ["JANELA", "AMARELO", "CARROÇA", "CAMISETA", "GIRAFA", "ESCADA", "MACACO", "PINTURA", "ABELHA", "CACHORRO", "MORANGO", "CHAVEIRO", "CADERNO", "TIGRE", "GALINHA", "DOCEIRA", "FORMIGA", "BATATA", "SORVETE", "COZINHA"],
+  3: ["COMPUTADOR", "HELICÓPTERO", "BIBLIOTECA", "BRINCADEIRA", "FOTOGRAFIA", "AVENTURA", "DINOSSAURO", "MARAVILHOSO", "DESCOBERTA", "RESPONSÁVEL", "CROCODILO", "ALIMENTAÇÃO", "CONHECIMENTO", "FELICIDADE", "PARALELEPÍPEDO", "EXTRAORDINÁRIO", "NATUREZA", "COMUNICATIVO", "TRANSPORTE", "PLANETA"]
+};
+
+const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+export function randomWord(level) {
+    const randomIndex = Math.floor(Math.random() * words[level].length)
+    const word = words[level][randomIndex];
+    return word
+}
+
+export function randomizesSecretWord(word) {
+    const wordLength = word.length;
+    const numberOfRandomizedCharacters = Math.floor(wordLength / 2);
+    let newWord = [];
+
+    for(let i = 0; i < wordLength; i++) newWord[i] = word[i]
+
+    for(let i = 0; i < numberOfRandomizedCharacters; i++) newWord[Math.floor(Math.random() * wordLength)] = "_";
+
+    return newWord;
+}
+
+export function randomizesResponseLetters(word, secretWord) {
+    const randomizedLetters = [];
+
+    for(let i = 0; i < word.length; i++) {
+        if(secretWord[i] == "_") {
+            randomizedLetters.push(word[i])
+        }
+        else {
+            const letter = alphabet[Math.floor(Math.random() * alphabet.length)];
+            randomizedLetters.push(letter);
+        }
+    }
+    randomizedLetters.sort();
+    return randomizedLetters;
+}
+
+export function determinesLevel(points) {
+    let level = 0
+    if (points >= 200) level = 3
+    else if (points >= 100) level = 2
+    else level = 1
+    
+    return level
+}
---
+++ script/main.js
@@ -0,0 +1,133 @@
+import { determinesLevel, randomizesSecretWord, randomizesResponseLetters, randomWord } from "./game.js";
+
+let gameScore = 200;
+let gameLevel = 0;
+let playerHearts = 5
+let word = "";
+let secretWord = "";
+const divLetters = document.querySelector("#letters");
+const h3Points = document.querySelector("#points")
+const divWord = document.querySelector("#word");
+const divRecords = document.querySelector("#records h3");
+
+gameLevel = determinesLevel(gameScore);
+word = randomWord(gameLevel);
+secretWord = randomizesSecretWord(word);
+
+// Desenha recordes
+drawRecords();
+
+// Desenha pontuação
+drawScore();
+
+// Desenha a palavra com caracteres ocultos
+drawSecretWord(secretWord);
+
+// Desenha as possibilidades de letras
+drawLetters();
+
+
+
+// Desenha corações
+const spanHearts = document.querySelector("#player span");
+for (let i = 0; i < playerHearts; i++) {
+    const imgHeart = document.createElement("img");
+    imgHeart.src = "assets/heart.png"
+    imgHeart.classList.add("hearts")
+    spanHearts.appendChild(imgHeart);
+}
+
+// Identifica click e acumula pontuação ou tira vida
+divLetters.addEventListener("click", (e) => {
+    const eventLetter = e.target.innerText;
+    // Se palavra contiver a letra clicada, secretWord é atualizada. Se não, vida é retirada.
+    if (e.target.tagName == "BUTTON") {
+        // Após o click, o elemento para de ser clicável
+        e.target.disabled = true;
+        e.target.style.pointerEvents = "none";
+        e.target.style.backgroundColor = "rgb(25, 25, 25)";
+
+        if (word.includes(eventLetter)) { // ADICIONAR: confirmação para saber se a letra já foi clicada
+            updateSecretWord(eventLetter);
+            drawSecretWord(secretWord);
+        } else {
+            playerHearts--;
+            spanHearts.removeChild(spanHearts.lastChild);
+            if (playerHearts == 0) {
+                endGame();
+            }
+        }
+    }
+
+    // Verifica se a palavra foi descoberta
+    if (word == secretWord.join("")) {
+        gameScore += 10;
+        drawScore();
+        setTimeout(() => {
+            gameLevel = determinesLevel(gameScore);
+            word = randomWord(gameLevel);
+            secretWord = randomizesSecretWord(word);
+            drawSecretWord();
+            drawLetters()
+        }, 1000)
+    }
+})
+
+
+function drawScore() {
+    h3Points.innerText = gameScore;
+}
+
+function drawRecords() {
+    if (localStorage.getItem("gameRecord") != null) {
+        const records = localStorage.getItem("gameRecord");
+        divRecords.innerText = records;
+    } else {
+        divRecords.innerText = "Sem recordes";
+    }
+}
+
+function drawSecretWord() {
+    const pWord = document.createElement("p");
+    divWord.innerHTML = "";
+    for (let letter of secretWord) pWord.innerText += `${letter} `; // Formata a exibição da palavra
+    divWord.appendChild(pWord);
+    divWord.style.display = "flex";
+}
+
+function drawLetters() {
+    const randomizedLetters = randomizesResponseLetters(word, secretWord);
+    divLetters.innerHTML = "";
+    for (let letter of randomizedLetters) {
+        const letterButton = document.createElement("button");
+        letterButton.classList.add("letter")
+        letterButton.innerHTML = letter;
+        divLetters.appendChild(letterButton);
+    }
+}
+
+function updateSecretWord(letter) {
+    for (let i = 0; i < secretWord.length; i++) {
+        if (secretWord[i] == "_" && word[i] == letter) {
+            secretWord[i] = letter;
+            break;
+        }
+    }
+}
+
+function setRecord() {
+    if (localStorage.getItem("gameRecord") == null) {
+        localStorage.setItem("gameRecord", gameScore);
+    } else {
+        localStorage.setItem(
+            "gameRecord", 
+            `${localStorage.getItem("gameRecord")}\n${gameScore}`
+        );
+    }
+
+}
+
+function endGame() {
+    setRecord();
+    drawRecords();
+}
